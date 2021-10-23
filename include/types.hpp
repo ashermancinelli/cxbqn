31e5a337322305755461058a4bf756901d3ff0c1
Asher Mancinelli <ashermancinelli@gmail.com>
2021-10-23 11:24:52 -0600

    Update types based on dzaimas layout

---
+++ include/types.hpp
@@ -0,0 +1,158 @@
+#pragma once
+#include <functional>
+#include <scalar_types.hpp>
+#include <type_traits>
+#include <variant>
+
+namespace cxbqn {
+
+// template deduction guides to make std::visit feel better
+template <class... Ts> struct overload : Ts... { using Ts::operator()...; };
+template <class... Ts> overload(Ts...) -> overload<Ts...>;
+
+// for nice enum->int casting
+template <typename E> constexpr auto to_underlying(E e) noexcept {
+  return static_cast<std::underlying_type_t<E>>(e);
+}
+
+namespace types {
+
+/**
+ *
+ * Type heirarchy:
+ * https://gist.github.com/dzaima/e7b24e10cf6ac33f62bf8cfd80758d4b
+ *
+ * Example output of cjs.bqn:
+ *
+ * \code{.sh}
+ * $ BQN ./ext/bqn/src/cjs.bqn '5+5'
+ * [[0,1,0,0,0,1,17,7],[runtime[0],5],[[0,1,0]],[[0,0]]]
+ * \endcode
+ *
+ * bytecode sequence = [0,1,0,0,0,1,17,7]
+ * constants = [runtime[0],5]
+ * blocks = [[0,1,0]]
+ * bodies = [[0,0]]
+ *
+ */
+
+struct Value {};
+
+struct Nothing : Value {};
+
+struct Character {
+  char v;
+};
+
+// Using cbqn's enum
+enum class NumType {
+  bit = 0,
+  i8 = 1,
+  i16 = 2,
+  i32 = 3,
+  f64 = 4,
+};
+
+struct Number : Value {
+  i32 refc;
+  NumType t;
+  union {
+    i8 _i8;
+    i16 _i16;
+    i32 _i32;
+    f64 _f64;
+  } v;
+};
+
+template <typename T> struct Array : Value {
+  std::vector<uz> shape;
+  T *values;
+  ~Array() {
+    if (sh != nullptr)
+      delete[] sh;
+  }
+};
+
+struct Reference : Value {
+  uz dis_from_parent;
+  uz position_in_parent;
+};
+
+struct Function : Value {};
+
+struct UserFn : Function {};
+
+struct Fork : Funciton {
+  Value *f, *g, *h;
+};
+
+struct Atop : Function {
+  Value *g, *h;
+};
+
+struct Md1 : Function {};
+
+struct Scope;
+struct UserMd1 : Md1 {
+  Scope sc;
+  Block bl;
+};
+
+struct Md2 : Function {};
+
+struct UserMd2 : Md2 {
+  Scope sc;
+  Block bl;
+};
+
+struct Md1Derived : Function {
+  Value *f, *m1;
+};
+
+struct Md2Derived : Function {
+  Value *f, *m2, *g;
+};
+
+struct Scope {
+  Scope *parent;
+  Value *vars[];
+};
+
+struct CompilationResult {
+  uz *bc; /// bytecode
+  Value *objs;
+};
+
+enum class BlockType {
+  func = 0,
+  md1 = 1,
+  md2 = 2,
+};
+
+struct Body {
+  uz bc_offset;
+  uz var_count;
+};
+
+/**
+ * Block without multiple bodies
+ */
+struct Block {
+  BlockType type;
+  bool immediate;
+  Comp *comp;
+  uz bc_offset;
+  uz var_count;
+};
+
+/**
+ * Block with multiple bodies
+ */
+struct MbBlock {
+  BlockType type;
+  bool immediate;
+  Comp *comp;
+};
+
+} // namespace types
+} // namespace cxbqn
