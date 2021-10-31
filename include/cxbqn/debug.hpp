01b861ba29314df2e5993c6c0147643850330310
Asher Mancinelli <ashermancinelli@gmail.com>
2021-10-31 15:22:21 -0600

    Nicer colors and vm output

--- include/cxbqn/debug.hpp
+++ include/cxbqn/debug.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include <string>
 #include <cxbqn/config.hpp>
 #include <cxbqn/fmt.hpp>
+#include <string>
 
 #ifdef CXBQN_DEEPCHECKS
 #include <spdlog/spdlog.h>
@@ -9,7 +9,11 @@
 #define CXBQN_DEBUG(...) spdlog::debug(__VA_ARGS__);
 #define CXBQN_CRIT(...) spdlog::critical(__VA_ARGS__);
 #define CXBQN_INFO(...) spdlog::info(__VA_ARGS__);
-#define CXBQN_SETLOGSTR() spdlog::set_pattern("cxbqn[%^%5l%$]:%v");
+#define CXBQN_SETLOGSTR()                                                      \
+  do {                                                                         \
+    spdlog::set_level(spdlog::level::CXBQN_LOGLEVEL);                                 \
+    spdlog::set_pattern("cxbqn[%^%5l%$]:%v");                                  \
+  } while (0);
 #else
 #define CXBQN_DEBUG(...)
 #define CXBQN_CRIT(...)
@@ -18,6 +22,6 @@
 #define CXBQN_SETLOGSTR()
 #endif
 
-#define CXBQN_STR_NC(x)                                                   \
+#define CXBQN_STR_NC(x)                                                        \
   static_cast<std::string>((nullptr == x) ? "null" : fmt::format("{}", *x))
 #define CXBQN_DEBUG_NC(fmt, x) CXBQN_DEBUG(fmt, CXBQN_STR_NC(x))
