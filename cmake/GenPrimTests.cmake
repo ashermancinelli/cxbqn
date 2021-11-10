4799c81992467bb415dcf803ab2657bdea557939
Asher Mancinelli <ashermancinelli@gmail.com>
2021-11-10 10:05:10 -0800

    Add more primitive tests, failing F=F

--- cmake/GenPrimTests.cmake
+++ cmake/GenPrimTests.cmake
@@ -6,8 +6,6 @@ if(HAS_BQN_EXE)
       "1e4≡5e3+5e3"
       "'c'≡'a'+2"
       "'a'≡¯2+'c'"
-      "'a'+'c'"
-      "F←-⋄f+2"
       "¯∞≡1e6-∞"
       "4≡-¯4"
       "¯∞≡-∞"
@@ -16,29 +14,50 @@ if(HAS_BQN_EXE)
       "@≡'a'-97"
       "3≡'d'-'a'"
       "'Q'≡'q'+'A'-'a'"
-      "97-'a'"
-  )
-  set(P_ANS
-      1
-      1
-      1
-      1
-      throws
-      throws
-      1
-      1
-      1
-      1
-      1
-      1
-      1
-      1
-      throws
+      "4≡÷0.25"
+      "∞≡÷0"
+      "0≡÷∞"
+      "1≡⋆0"
+      "¯1≡¯1⋆5"
+      "1≡¯1⋆¯6"
+      "3≡⌊3.9"
+      "¯4≡⌊¯3.9"
+      "∞≡⌊∞"
+      "¯∞≡⌊¯∞"
+      "¯1e30≡⌊¯1e30"
+      "1≡1=1"
+      "0≡¯1=∞"
+      "1≡'a'='a'"
+      "0≡'a'='A'"
+      "1≡{F←+⋄f=f}"
+      "1≡{a‿b←⟨+´,+´⟩⋄a=b}"
+      "0≡{_op←{𝕗}⋄op='o'}"
+      "0≡{F←{𝕩}⋄G←{𝕩}⋄f=g}"
+      "1≡{F←{𝕩}⋄f=f}"
+      "1≡1≤1"
+      "1≡¯∞≤¯1e3"
+      "0≡∞≤¯∞"
+      "1≡∞≤@"
+      "0≡'z'≤¯0.5"
+      "1≡'a'≤'a'"
+      "0≡'c'≤'a'"
+      "⟨⟩≡≢<2"
+      "⟨3⟩≡≢\\\"abc\\\""
+      "⟨2,3⟩≡≢>\\\"abc\\\"‿\\\"fed\\\""
+      "⟨2,3,4,5⟩≡≢2‿3‿4‿5⥊↕120"
+      "⟨6⟩≡≢⥊>\\\"abc\\\"‿\\\"fed\\\""
+      "\\\"abc\\\"≡0⊑\\\"abc\\\"‿\\\"de\\\""
+      "\\\"de\\\"≡1⊑\\\"abc\\\"‿\\\"de\\\""
+      "⟨⟩≡↕0"
+      "⟨0⟩≡↕1"
+      "⟨0,1,2,3,4,5,6⟩≡↕7"
+      "1≡!1"
+      "1≡'e'!1"
   )
   set(P_TEST_SOURCE "${PROJECT_BINARY_DIR}/test_prim.cpp")
   init_gen_file(${P_TEST_SOURCE})
 
-  foreach(test ans IN ZIP_LISTS P_TESTS P_ANS)
+  foreach(test ${P_TESTS})
     execute_process(
       COMMAND
         ${BASH} -c
@@ -50,32 +69,25 @@ if(HAS_BQN_EXE)
       APPEND ${P_TEST_SOURCE}
       "
 TEST_CASE(\"${test}\") {
-  spdlog::critical(\"test='{}', ans='{}'\", \"${test}\", \"${ans}\");
+  spdlog::critical(\"test='{}'\", \"${test}\");
   const auto rt = provides::get_runtime();
   const auto runtime = rt->values;
   CompileParams p{ ${compiled_test} };
 "
     )
 
-    if("${ans}" STREQUAL "throws")
-      file(
-        APPEND ${P_TEST_SOURCE}
-        "\n  REQUIRE_THROWS(vm::run(p.bc, p.consts.v, p.blk_defs, p.bodies));}\n"
-      )
-    else()
-      file(
-        APPEND ${P_TEST_SOURCE}
-        "
+    file(
+      APPEND ${P_TEST_SOURCE}
+      "
   auto ret = vm::run(p.bc, p.consts.v, p.blk_defs, p.bodies);
   REQUIRE(nullptr != ret.v);
   REQUIRE(nullptr != ret.scp);
   Number *n = dynamic_cast<Number *>(ret.v);
   REQUIRE(nullptr != n);
-  CHECK(${ans} == doctest::Approx(n->v));
+  CHECK(1 == doctest::Approx(n->v));
 }
 "
-      )
-    endif()
+    )
   endforeach()
 else()
   message(
