59f37711f8226c5156773d0ff5ab44b5324f2454
Asher Mancinelli <ashermancinelli@gmail.com>
2021-12-22 15:32:15 -0700

    Add enlist to native r0

---
+++ src/rt_native/r0/enlist_pair.cpp
@@ -0,0 +1,14 @@
+#include <cxbqn/rt_native.hpp>
+
+namespace cxbqn::rt_native::r0 {
+
+O<Value> EnlistPair::call(u8 nargs, Args &args) {
+  auto ret = CXBQN_NEW(Array);
+  if (2 == nargs)
+    ret->values.push_back(args[2]);
+  ret->values.push_back(args[1]);
+  ret->shape.push_back(ret->values.size());
+  return ret;
+}
+
+} // namespace cxbqn::rt_native::r0
--- src/rt_native/replace_with_native.cpp
+++ src/rt_native/replace_with_native.cpp
@@ -5,12 +5,14 @@ namespace cxbqn::rt_native {
 using namespace cxbqn;
 using namespace cxbqn::types;
 
+// ⌊ ⌈ | < > ≠ ≥ ⊢ ⊣ ∾ ⋈ ↑ ↓ ⊏ ˙ ˜ ¨ ´ ∘ ○ ⊸ ⟜ ◶ ⍟
 void replace_r0_with_native(O<Array> r0) {
-  r0->values[5] = CXBQN_NEW(r0::NE);        // ≠
-  r0->values[7] = CXBQN_NEW(r0::Rtack);     // ⊢
-  r0->values[8] = CXBQN_NEW(r0::Ltack);     // ⊣
-  r0->values[9] = CXBQN_NEW(r0::JoinTo);    // ∾
-  r0->values[15] = CXBQN_NEW(r0::SelfSwap); // ˜
+  r0->values[5] = CXBQN_NEW(r0::NE);          // ≠
+  r0->values[7] = CXBQN_NEW(r0::Rtack);       // ⊢
+  r0->values[8] = CXBQN_NEW(r0::Ltack);       // ⊣
+  r0->values[9] = CXBQN_NEW(r0::JoinTo);      // ∾
+  r0->values[10] = CXBQN_NEW(r0::EnlistPair); // ⋈
+  r0->values[15] = CXBQN_NEW(r0::SelfSwap);   // ˜
 }
 
 void replace_r1_with_native(O<Array> r1) {}
