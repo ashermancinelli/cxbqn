1cf8f82d3828cfa02f3e6af3bea38acac5924e34
Asher Mancinelli <ashermancinelli@gmail.com>
2021-12-23 16:06:47 -0700

    Fix bug in ¨

--- src/rt_native/r0/each.cpp
+++ src/rt_native/r0/each.cpp
@@ -7,8 +7,11 @@ O<Value> Each::call(u8 nargs, Args &args) {
   auto x = dyncast<Array>(args[1]);
   auto w = dyncast<Array>(args[2]);
 
-  const auto l = x->shape[0];
+   // ,m1(f=>(x,w)=>arr(x.map((e,i)=>call(f,e,w[i])),x.sh))    // ¨
+   //
+  const auto l = x->N();
   auto ret = CXBQN_NEW(Array, l);
+  ret->shape = x->shape;
   for (int i = 0; i < l; i++) {
     Args a{f, x->values[i], w->values[i]};
     ret->values[i] = f->call(2, a);
