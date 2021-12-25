d7d8c2d7949676f8903a8b913ef126f021c093ca
Asher Mancinelli <ashermancinelli@gmail.com>
2021-12-25 10:07:25 -0800

    Install bqn libraries, check install for •Import

---
+++ lib/README.md
@@ -0,0 +1,6 @@
+## CXBQN libraries
+
+Some of these libraries are taken from Marshall's bqn-libs repo.
+CXBQN does not have support for headers at the time of writing, so they have been modified to not use headers.
+The libraries are also sometimes broken into very small scripts to make debugging CXBQN easier.
+In the future, we intend to support the official bqn-libs.
--- src/sys/import.cpp
+++ src/sys/import.cpp
@@ -1,37 +1,93 @@
 #include "sys_helper.hpp"
+#include <cstdlib>
+#include <cxbqn/config.hpp>
+
+namespace {
+static inline auto resolve_bqnpath(const fs::path &p) -> fs::path {
+  auto const *e = std::getenv("BQNPATH");
+  if (nullptr == e)
+    return "";
+
+  // : delimited environment var
+  const auto bqnpath = std::string(e);
+  uz start = 0, end;
+  std::string elem;
+  fs::path pelem;
+
+  // Try to split by ':' first
+  while ((end = bqnpath.find(':', start)) != std::string::npos) {
+    elem = bqnpath.substr(start, end);
+    if (fs::exists(pelem = (fs::path(elem) / p)))
+      return pelem;
+    start = end;
+  }
+
+  // If no : was found, maybe it's a single path and we can just return that
+  if (bqnpath.size() and fs::exists(pelem = (bqnpath / p)))
+    return pelem;
+
+  // Try the CXBQN installation prefix as well
+  if (fs::exists(pelem = (fs::path(CXBQN_INSTALL_PREFIX) / p)))
+    return pelem;
+
+  // Otherwise, just return the default directory
+  return fs::path("/usr/share/bqn") / p;
+}
+} // namespace
+
 namespace cxbqn::sys {
 
 O<Value> Import::call(u8 nargs, Args &args) {
   CXBQN_DEBUG("•Import: nargs={},args={}", nargs, args);
 
-  auto x = dyncast<Array>(args[1]);
-  if (!x)
-    throw std::runtime_error("•Import: 𝕩 must be a string");
+  const auto x = [=] {
+    const auto xx = dyncast<Array>(args[1]);
+    if (!xx)
+      throw std::runtime_error("•Import: 𝕩 must be a string");
+    const auto s = xx->to_string();
+    if (!s.size())
+      throw std::runtime_error("•Import: got empty 𝕩");
+    return s;
+  }();
 
   auto w = 2 == nargs ? dyncast<Array>(args[2]) : CXBQN_NEW(Array);
 
-  auto basepath = fs::path(_path->to_string());
-  auto f = basepath / fs::path(x->to_string());
-  if (!fs::exists(f)) {
-    std::stringstream ss;
-    ss << "•Import path " << f.c_str() << " does not exist";
-    throw std::runtime_error(ss.str());
-  }
+  const auto f = [=, path = fs::path(_path->to_string()), &x]() -> fs::path {
+    // We got an absolute path
+    if ('/' == x[0])
+      return x;
 
-  std::FILE *fp = std::fopen(f.c_str(), "r");
-  if (!fp)
-    throw std::runtime_error("•Import: could not open path");
+    // Otherwise, use relative path based on directory of script
+    const auto relative = path / fs::path(x);
+    if (fs::exists(relative))
+      return relative;
 
-  std::string _src = "";
-  int ch;
-  while ((ch = fgetc(fp)) != EOF) {
-    _src += ch;
-  }
-  std::fclose(fp);
-  auto src = CXBQN_NEW(Array, _src);
+    const auto from_bqnpath = resolve_bqnpath(x);
+    if (fs::exists(from_bqnpath))
+      return from_bqnpath;
 
-  auto sys_func_rslvr = CXBQN_NEW(SystemFunctionResolver, w, _fmt, _repr,
-                                  _compiler, _bqnruntime, _path);
+    const auto err = fmt::format("•Import: path {} does not exist", x);
+    throw std::runtime_error(err);
+  }();
+
+  const auto src = [&f]() -> O<Array> {
+    std::FILE *fp = std::fopen(f.c_str(), "r");
+    if (!fp)
+      throw std::runtime_error("•Import: could not open path");
+
+    std::string _src = "";
+    int ch;
+    while ((ch = fgetc(fp)) != EOF) {
+      _src += ch;
+    }
+    std::fclose(fp);
+
+    return CXBQN_NEW(Array, _src);
+  }();
+
+  auto sys_func_rslvr =
+      CXBQN_NEW(SystemFunctionResolver, w, _fmt, _repr, _compiler, _bqnruntime,
+                CXBQN_NEW(Array, f.parent_path()));
 
   auto compw = CXBQN_NEW(Array, 2);
   compw->values[0] = _bqnruntime;
