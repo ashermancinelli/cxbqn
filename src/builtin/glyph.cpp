9cc691a91db635b9672025010586603b12a99843
Asher Mancinelli <ashermancinelli@gmail.com>
2021-12-28 13:50:37 -0700

    Better formatting strategy with •glyph

--- src/builtin/glyph.cpp
+++ src/builtin/glyph.cpp
@@ -8,15 +8,17 @@ O<Value> Glyph::call(u8 nargs, Args &args) {
   static const std::string glyphs{
       "+-×÷⋆√⌊⌈|¬∧∨<>≠=≤≥≡≢⊣⊢⥊∾≍⋈↑↓↕«»⌽⍉/⍋⍒⊏⊑⊐⊒∊⍷⊔!˙˜˘¨⌜⁼´˝`∘○⊸⟜⌾⊘◶⎉⚇⍟⎊"};
   auto x = args[1];
-  if (t_Namespace == type_builtin(x)) {
-    auto xx = dyncast<Namespace>(x);
+
+  auto it = std::find(runtime.begin(), runtime.end(), x);
+
+  // If we can't find it in the runtime, we want to use the native formatting
+  // method
+  if (it == runtime.end()) {
     std::stringstream ss;
     x->repr(ss);
     return CXBQN_NEW(Array, ss.str());
   }
-  auto it = std::find(runtime.begin(), runtime.end(), x);
-  if (it == runtime.end())
-    return CXBQN_NEW(Character, U'?');
+
   auto glyph = glyphs.begin();
   utf8::advance(glyph, std::distance(runtime.begin(), it), glyphs.end());
   return CXBQN_NEW(Character, (c32)utf8::next(glyph, glyphs.end()));
