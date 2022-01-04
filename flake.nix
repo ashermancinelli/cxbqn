e207d94295936dff5a4c880e6d64604f40fedcde
Asher Mancinelli <ashermancinelli@gmail.com>
2022-01-04 04:05:28 +0000

    update flakes

--- flake.nix
+++ flake.nix
@@ -12,11 +12,13 @@
           url = "https://github.com/ashermancinelli/cxbqn.git";
           rev = "4816322";
           sha256 = "sha256-ruf4ycwSyJ36eczA4n1cdokRdO6X/+PfcOUnEK/i3as=";
+          fetchSubmodules = true;
         };
         nativeBuildInputs = with pkgs; [
           gcc11
           cmake
         ];
+        buildPhase = "make -j $NIX_BUILD_CORES";
       };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.cxbqn;
